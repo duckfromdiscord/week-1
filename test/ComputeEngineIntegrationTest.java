caff87bc0650c52ed89190a2eecb7951aec830a2
duck <113956421+duckfromdiscord@users.noreply.github.com>
2025-02-19 21:55:36 -0500

    Compute engine integration test

---
+++ test/ComputeEngineIntegrationTest.java
@@ -0,0 +1,38 @@
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ComputeEngineIntegrationTest {
+	
+	private ArrayList<Integer> slowSort(ArrayList<Integer> inputData) {
+		inputData.sort((e1, e2) -> e1.compareTo(e2));
+		return inputData;
+	}
+	
+	@Test
+	public void computeEngineIntegrationTest() {
+		
+		DatabaseMemoryImplementation inMemoryDB = new DatabaseMemoryImplementation();
+		TestInputConfig initialInput = new TestInputConfig();
+		// TODO: inMemoryDB should take the initialInput
+		
+		ArrayList<Integer> inputData = new ArrayList<Integer>();
+		inputData.addAll(initialInput.getInputData());
+		
+		// Create our sorter
+		BozoSorter bozoSorter = new BozoSorter();
+		bozoSorter.setRandomSeed(1);
+		
+		int index = inputData.removeFirst();
+		WrappedArrayList<Integer> wrappedData = new WrappedArrayList<Integer>();
+		wrappedData.setItems(inputData);
+		bozoSorter.setData(wrappedData, index);
+		bozoSorter.sortUntilFinished();
+		
+		Assertions.assertEquals(bozoSorter.getData().getItemAtIndex(index), slowSort(inputData).get(index));
+		
+	}
+	
+}
