d167cdde81dbdbb7b657de23c973fd8075013f38
duck <113956421+duckfromdiscord@users.noreply.github.com>
2025-02-19 19:50:36 -0500

    conceptual API placeholder

---
+++ src/BozoSorter.java
@@ -0,0 +1,37 @@
+
+public class BozoSorter implements Sorter<Integer> {
+
+	private WrappedArrayList<Integer> data;
+	private DatabaseSeverInterface database;
+	
+	@Override
+	public boolean step() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean checkSorted() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean setData(WrappedArrayList<Integer> data, int index) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public WrappedArrayList<Integer> getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean sortUntilFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
