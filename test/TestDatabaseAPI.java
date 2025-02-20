226eddc0f4d7605ed371ad0d9f7f4fe9408b8d75
ImEvwan <61602941+ImEvwan@users.noreply.github.com>
2025-02-19 21:19:20 -0500

    style fix

--- test/TestDatabaseAPI.java
+++ test/TestDatabaseAPI.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class testDatabaseAPI {
+public class TestDatabaseAPI {
 	@Test 
 	public void testAPIs() {
 		DatabaseServer server = new DatabaseServer();
