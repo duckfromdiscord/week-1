e7973f654be2339581e98a5f33b003654adbfd01
duck <113956421+duckfromdiscord@users.noreply.github.com>
2025-02-27 18:59:37 -0500

    implement compute engine functions

--- src/BozoSorter.java
+++ src/BozoSorter.java
@@ -6,40 +6,65 @@ public class BozoSorter implements Sorter<Integer> {
 	private DatabaseSeverInterface database;
 	private Random random;
 	
+	private boolean swapItems(int item1, int item2) {
+		int data1 = data.getItemAtIndex(item1);
+		int data2 = data.getItemAtIndex(item2);
+		data.set(item1, data2);
+		data.set(item2, data1);
+		return true;
+	}
+	
 	@Override
 	public boolean step() {
-		// TODO Auto-generated method stub
-		return false;
+		int item1 = random.nextInt(0, data.size());
+		int item2 = random.nextInt(0, data.size());
+		return this.swapItems(item1, item2);
 	}
 
 	@Override
 	public boolean checkSorted() {
-		// TODO Auto-generated method stub
-		return false;
+		// Edge case: if there are no items the array is sorted
+		if (data.size() < 1) {
+			return true;
+		}
+		
+		// since we checked, this should never be null
+		int lastValue = data.getItemAtIndex(0);
+		
+		for (int i = 1; i < data.size(); i++) {
+			// check if ascending
+			if (data.getItemAtIndex(i) < lastValue) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 
 	@Override
 	public boolean setData(WrappedArrayList<Integer> data, int index) {
-		// TODO Auto-generated method stub
-		return false;
+		this.data = data;
+		return true;
 	}
 
 	@Override
 	public WrappedArrayList<Integer> getData() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.data;
 	}
 
 	@Override
 	public boolean sortUntilFinished() {
-		// TODO Auto-generated method stub
-		return false;
+		while (!this.checkSorted()) {
+			this.step();
+		}
+		return this.checkSorted();
 	}
 
 	@Override
 	public boolean setRandomSeed(long seed) {
-		// TODO Auto-generated method stub
-		return false;
+		this.random = new Random();
+		this.random.setSeed(seed);
+		return true;
 	}
 
 }
