0d9dbe4f197f0e4da37282225d830b9e5d6bddac
ImEvwan <61602941+ImEvwan@users.noreply.github.com>
2025-03-06 20:03:39 -0500

    Assignment 5
    Added exception handling on the file interaction (database input) and the user input for username. For database input, it was important to ensure that the file existed, which Scanner has a built in exception for that. You just had to catch it and handle it. For the client side of this, I had to ensure that they wont input a blank, whitespace only, or null username as that's a bad time.
    
    Other than that, I made some changes in the actual API, mostly for readability. Variables were renamed appropriately and comments were left on confusing sections of code.

--- test/TestDatabaseAPI.java
+++ test/TestDatabaseAPI.java
@@ -9,11 +9,21 @@ import org.junit.jupiter.api.Test;
 public class TestDatabaseAPI {
 	@Test 
 	public void testAPIs() {
-		DatabaseServer server = new DatabaseServer(new LoginRequest("Evan"));
+		DatabaseServer server = null; 
+		//Need to declare this outside, otherwise it would be local to the catch block
+		
+		//Error Logic Integration Test
+		try {
+			server = new DatabaseServer(new LoginRequest("Evan"));
+			System.out.println("LoginRequest Created: " + server.getUserID());
+		} catch (IllegalArgumentException e) { //Handles the exception to make troubleshooting easier for the client
+			System.err.println("Failed to create request: " + e);
+		}
+		
 		boolean storeUserData = server.storeUserData("src/data.csv");
 		Assertions.assertEquals(storeUserData, true);
 		
-		Assertions.assertEquals(server.processData(0), true); //New Integration test 2.27.2025. Tests methods implemented in DatabaseServer.
+		Assertions.assertEquals(server.processData(0), true); //New Integration test
 		
 		
 		LoginRequest mockRequest = Mockito.mock(LoginRequest.class);
